febfcfd7bce7a55ca0963c93be98e73e31647905
j19015 <j19015@sangi.jp>
2023-02-24 12:49:47 +0900

    add useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 const IndexPage=()=>{
-  return <img src="https://cdn2.thecatapi.com/images/bpc.jpg" />;
+  const [catImageUrl,setImageUrl]=useState("https://cdn2.thecatapi.com/images/bpc.jpg");
+  return <img src={catImageUrl}/>;
 }
 export default IndexPage;
