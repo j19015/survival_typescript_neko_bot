13667ac0c97736e4a20f171a2a3c96996f4b2d37
j19015 <j19015@sangi.jp>
2023-02-24 13:00:08 +0900

    button click random cat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import { useState } from "react";
+
+const catImages: string[] = [
+  "https://cdn2.thecatapi.com/images/bpc.jpg",
+  "https://cdn2.thecatapi.com/images/eac.jpg",
+  "https://cdn2.thecatapi.com/images/6qi.jpg",
+];
+
+const RandomCatImage=(): string =>{
+  const index=Math.floor(Math.random()*catImages.length);
+  return catImages[index];
+};
+
+
 const IndexPage=()=>{
-  const [catImageUrl,setImageUrl]=useState("https://cdn2.thecatapi.com/images/bpc.jpg");
-  return <img src={catImageUrl}/>;
+  const [catImageUrl,setCatImageUrl]=useState("https://cdn2.thecatapi.com/images/bpc.jpg");
+
+  const handleClick=()=>{
+    setCatImageUrl(RandomCatImage());
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>今日のニャンコ</button>
+      <div style={{marginTop:8}}>
+        <img src={catImageUrl}/>
+      </div>
+    </div>
+  )
 }
+
 export default IndexPage;
